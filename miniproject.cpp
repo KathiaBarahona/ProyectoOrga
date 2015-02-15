f3d84c91c20aa032cd7d31b2306ab285552296be
kathybarahona03 <kathia.barahona@unitec.edu>
2015-02-14 23:48:53 -0600

    Borrar version 1

--- miniproject.cpp
+++ miniproject.cpp
@@ -11,14 +11,15 @@ struct Campo{
 	char tipo[7];
 	int tamano;
 };
+int offset(int,vector<Campo>);
 int getStructure(const char*, vector<Campo>&);
 void Header(const char*, vector<Campo>&);
 void Agregar(const char*,vector<Campo>);
 void Listar(const char*, vector<Campo>);
-void Borrar();
-void Modificar();
-void Compactar();
-void Buscar();
+void Borrar(const char*, vector<Campo>);
+void Modificar(const char*,vector<Campo>);
+void Compactar(const char*,vector<Campo>);
+void Buscar(const char*, vector<Campo>);
 
 union charint{
 	char raw[sizeof(int)];
@@ -49,16 +50,26 @@ int main(int argc, char* argv[]){
 	 
 	}
 	int opcion2=0;
-	while(opcion2 != 3){
-		cout << "1.Agregar Datos\n2.Listar Datos\n3.Salir" << endl;
+	while(opcion2 != 7){
+		cout << "1.Agregar Datos\n2.Listar Datos\n3.Buscar Registro\n4.Borrar\n5.Compactar\n6.Modificar\n7.Salir" << endl;
 		cin >> opcion2;
 		if(opcion2==1){
 			Agregar(nbin, registros);
-		}else{
-			if(opcion2==2){
-				cout << nbin << endl;
-				Listar(nbin, registros);
-			}
+		}
+		if(opcion2==2){
+			Listar(nbin, registros);
+		}
+		if(opcion2==3){
+			Buscar(nbin, registros);
+		}
+		if(opcion2==4){
+			Borrar(nbin, registros);
+		}
+		if(opcion2==5){
+			Compactar(nbin, registros);
+		}
+		if(opcion2==6){
+			Modificar(nbin, registros);
 		}
 	}
 	return 0;
@@ -149,46 +160,81 @@ void Listar(const char* nbin, vector<Campo> registros){
 	char b[sizeof(int)];
 	in.read(b, sizeof(int));
 	memcpy(cbyte.raw,b,sizeof(int));
-	//in.clear();
-	cbyte.num;
 	in.seekg(cbyte.num,ios::beg);
+	//cout << cbyte.num << endl;
+	int pos=1;
 	while(true){
-		if(i==registros.size())
+		if(i==registros.size()){
+			cout << endl;
 			i=0;
+			pos++;
+		}
 		if(registros[i].tipo=="Entero"){
 			char buffer[sizeof(int)];
 			if(!in.read(buffer,sizeof(int)))
 				break;
+			if(i==0)
+				cout << pos << ". ";
 			cout << registros[i].nombre <<"-";
 			memcpy(ci.raw,buffer,sizeof(int));
-			cout << ci.num << endl;
+			if(i != registros.size()-1){
+				cout << ci.num << ",";
+			}else{
+				cout << ci.num;
+			}
 
 		}else{
 			char buffer[registros[i].tamano-1];
 			if(!in.read(buffer,registros[i].tamano-1))
 				break;
+			if(i==0)
+				cout << pos << ". ";
 			cout << registros[i].nombre << "-";
 			buffer[registros[i].tamano-1]='\0';
-			cout << buffer << endl;
+			if(i != registros.size()-1){
+				cout << buffer << ",";
+			}else{
+				cout << buffer;
+			}
 		}
 		i++;
 	}	
 	in.close();	
 
-}
+}//Lista los registros
 
 
 void Borrar(const char* nbin, vector<Campo> registros){
 	Listar(nbin, registros);
-	int offset;
-	cout << "Ingrese el offset del registro a eliminar" << endl;
-	cin >> offset;
-	ofstream out(nbin,ios::out|ios::binary|ios::app);
+	int rrn;
+	cout << "Ingrese el numero del registro a eliminar" << endl;
+	cin >> rrn;
+	rrn=rrn-1;
+	int os = offset(rrn, registros);
+	charint cbyte;
+	fstream in(nbin,ios::in|ios::binary|ios::out);
+	char b[sizeof(int)];
+	in.read(b, sizeof(int));
+	memcpy(cbyte.raw,b,sizeof(int));
+	os=os+cbyte.num;
+	in.seekp(os);
+	in.write("*",1);
+	in.close();
+}//Borra al agregar un * en el primer byte del registro
+int offset(int rrn,vector<Campo> registros){
+	int size=0;
+	for(int i=0; i < registros.size() ; i++){
+		if(registros[i].tipo == "Entero")
+			size+=sizeof(int);
+		else
+			size = size + registros[i].tamano -1;
+	}
+	return rrn*size;
 }
-void Modificar(){
+void Modificar(const char* nbin, vector<Campo> registros){
 }
-void Buscar(){
+void Buscar(const char* nbin, vector<Campo> registros){
 }
-void Compactar(){
+void Compactar(const char* nbin, vector<Campo> registros){
 }
 
